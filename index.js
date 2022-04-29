87f56296e20042f5a89c7f6a75bc31cdb1c12bb4
pravinvilaskale <pravinvkale889@gmail.com>
2022-04-29 23:27:23 +0530

    Changes
    Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose') 
 const app = express()
-const port = 3000
+require('dotenv').config();
 const {mogoUrl} = require('./keys')
 
 
