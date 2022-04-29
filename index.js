8a7bb83715f5fb479cde74421293f30c2c90e638
pravinvilaskale <pravinvkale889@gmail.com>
2022-04-30 00:00:42 +0530

    Change
    Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose') 
 const app = express()
-require('dotenv').config();
+const PORT = process.env.PORT || 5000
 const {mogoUrl} = require('./keys')
 
 
@@ -38,4 +38,4 @@ app.get('/',auth,(req,res)=>{
     res.send("Your email is "+req.user.email)
 })
 
-app.listen(process.env.PORT || 5000)
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
